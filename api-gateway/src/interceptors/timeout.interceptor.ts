50e71dc8f6e8b7c499702b328ce0e340775ecfbc
Marco Antonio Pandolfo <marcopandolfo577@gmail.com>
2021-07-25 03:17:04 -0300

    feat(api-gateway): Criado TimeoutInterceptor

---
+++ api-gateway/src/interceptors/timeout.interceptor.ts
@@ -0,0 +1,15 @@
+import {
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  Injectable,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { timeout } from 'rxjs/operators';
+
+@Injectable()
+export class TimeoutInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(timeout(10000));
+  }
+}
--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as momentTimezone from 'moment-timezone';
 import { AllExceptionFilter } from './filters/http-exception.filter';
+import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalInterceptors(new TimeoutInterceptor());
   app.useGlobalFilters(new AllExceptionFilter());
 
   Date.prototype.toJSON = function(): any {
